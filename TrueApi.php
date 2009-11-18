a027c627c12e90fddf01d87b8c79ca8f5e4fefb6
Kevin van Zonneveld <kevin@vanzonneveld.net>
2009-11-18 19:24:30 +0100

    Basic support for REST login

--- TrueApi.php
+++ TrueApi.php
@@ -1,14 +1,7 @@
 <?php
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
 if (!defined('DIR_TRUEAPI')) {
     define('DIR_TRUEAPI', dirname(__FILE__));
 }
-
 
 if (!defined('DIR_RESTCLIENT_ROOT')) {
 	if (is_dir('/home/kevin/workspace/rest_client')) {
@@ -18,15 +11,26 @@ if (!defined('DIR_RESTCLIENT_ROOT')) {
 	}
 }
 
+require_once DIR_TRUEAPI.'/TrueApiModel.php';
 require_once DIR_RESTCLIENT_ROOT.'/RestClient.php';
 
 class TrueApi extends RestClient {
-    public $request_prefix = 'http://admin.true.dev/';
-    protected $_controllers = array(
+    public $request_prefix = 'http://admin.true.dev/cakephp/';
+    public $request_suffix = '.json';
+    public $user_agent     = 'True Api 0.1';
+    protected $auth        = array();
+
+    public function setAuth($username, $password, $apikey, $class = 'Customer') {
+        $auth  = compact('username', 'password', 'apikey', 'class');
+        $query = http_build_query($auth);
+        $this->curl->headers['Authorization'] = sprintf('TRUEREST %s', $query);
+    }
+    
+    protected $controllers = array(
         'servers',
         'dns_domains',
     );
-
+    
     public static function classify ($lowerCaseAndUnderscoredWord) {
         return str_replace(" ", "", ucwords(str_replace("_", " ", $lowerCaseAndUnderscoredWord)));
     }
@@ -36,13 +40,18 @@ class TrueApi extends RestClient {
         $this->add_response_type('json', array(get_class($this), 'parseJson'), '.json');
         $this->add_response_type('xml', array(get_class($this), 'parseXml'), '.xml');
 
-        foreach ($this->_controllers as $controller) {
+        $this->set_response_type('json');
+
+        foreach ($this->controllers as $controller) {
             $model = TrueApi::classify($controller);
             $this->{$model} = new TrueApiModel($controller, $this);
         }
     }
 
     public function parseJson($curl_response) {
+    
+        prd(compact('curl_response'));
+
         return json_decode($curl_response, true);
     }
 
--- TrueApiModel.php
+++ TrueApiModel.php
@@ -1,14 +1,19 @@
 <?php
 class TrueApiModel {
     public $TrueApi;
+    public $controller;
     public function __construct($controller, $apiObject) {
         $this->TrueApi = $apiObject;
+        $this->controller = $controller;
     }
 
-    public function  __call($name,  $arguments) {
-        prd($name);
-        $data = $this->get(sprintf('%s/%s', $arguments[0], $name));
-        return $data;
+    protected function _get($action, $vars) {
+        $path = sprintf('%s/%s', $this->controller, $action);
+        return $this->TrueApi->get($path, $vars);
+    }
+
+    public function index($vars = array()) {
+        return $this->_get(__FUNCTION__, $vars);
     }
 }
 ?>
